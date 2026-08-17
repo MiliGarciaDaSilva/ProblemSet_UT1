faf30fae260d7bfe5ea7bae8233f9503493ea923
fedehmartinez <federicoh.martinez@correo.ucu.edu.uy>
2026-08-17 19:24:35 -0300

    Rerources arreglado.

--- src/main/java/ucu/edu/aed/Ejercicio7/MainEj7.java
+++ src/main/java/ucu/edu/aed/Ejercicio7/MainEj7.java
@@ -3,7 +3,7 @@ package ucu.edu.aed.Ejercicio7;
 public class MainEj7 {
   public static void main(String[] args) {
     Funcion f = new Funcion();
-    f.cargarDesdeArchivo("src\\main\\java\\ucu\\edu\\aed\\resources\\numeros.txt");
+    f.cargarDesdeArchivo("ProblemSet_UT1\\src\\main\\java\\ucu\\edu\\aed\\resources ej7\\numeros.txt");
     f.ordenar();
     f.mostrarResultados();
   }
