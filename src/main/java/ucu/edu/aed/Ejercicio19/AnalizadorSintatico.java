5cb7d4a7fe5d7b20cc19d787dc5804a6ed46362b
Santiago Acosta <santiagon.acosta@correo.ucu.edu.uy>
2026-08-19 20:04:13 -0300

    java del ejercicio 19 hecho

--- src/main/java/ucu/edu/aed/Ejercicio19/AnalizadorSintatico.java
+++ src/main/java/ucu/edu/aed/Ejercicio19/AnalizadorSintatico.java
@@ -5,6 +5,28 @@ import ucu.edu.aed.implementaciones.Pila;
 
 public class AnalizadorSintatico {
 
+    public static boolean controlCorchetes(String caracteres){
+        Pila<Character> pila = new Pila<>();
 
-    
+        if (caracteres == null){
+            return true;
+        }
+
+        for (int i = 0; i < caracteres.length(); i++){
+            char actual = caracteres.charAt(i);
+
+            if (actual == '{'){
+                pila.mete(actual);
+            }
+            else{
+                if (actual == '}'){
+                    if (pila.esVacio()){
+                        return false;
+                    }
+                    pila.saca();
+                }
+            }
+        }
+        return true;
+    }
 }
